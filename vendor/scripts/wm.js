def2fd9c06a0f9885966dcec3946e96add1e5c0a
Rishi Kothari <itsrishikothari@gmail.com>
2022-01-01 22:29:45 -0500

    update responsivity for wm

--- vendor/scripts/wm.js
+++ vendor/scripts/wm.js
@@ -8,11 +8,9 @@ function randomInteger(min, max) {
 const resizeListener = window.matchMedia("(max-width: 1200px)");
 
 if (resizeListener.matches) {
-    console.log("hi");
-    windows.forEach((window) => {
-        e.style.setProperty("left", `50%`);
-        e.style.setProperty("transform", "translateX(-50%)");
-        e.style.setProperty("top", `auto`);
+    windows.forEach((e) => {
+        e.style.setProperty("transform", "translate(0, -50%)");
+        e.style.setProperty("top", `50%`);
     });
 }
 
@@ -20,22 +18,24 @@ windows.forEach((e) => {
     e.style.zIndex = maxZindex + 1;
     maxZindex += 1;
 
-    e.style.setProperty("left", `${randomInteger(30, 70)}%`);
-    e.style.setProperty("top", `${randomInteger(10, 70)}%`);
+    if (!resizeListener.matches) {
+        e.style.setProperty("left", `${randomInteger(30, 70)}%`);
+        e.style.setProperty("top", `${randomInteger(10, 70)}%`);
+    }
+
     e.firstElementChild.firstElementChild.addEventListener(
         "click",
         (ev) => {
             ev.preventDefault();
-            console.log("hi");
             e.classList.add("off");
             e.classList.remove("on");
             const id = e.dataset.mi;
-            console.log(id);
+
             queryElementDatum("for", id).forEach((spanElement) => {
                 spanElement.classList.remove("active");
                 console.log(spanElement);
             });
         }
     );
-    // dragElement(e);
 });
+
